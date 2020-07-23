d15dd6e62bd57033ed238b56df7d6f748a99f802
icannotdothecode <vishals0705@gmail.com>
2020-07-22 18:31:05 -0700

    started new project

--- main.py
+++ main.py
@@ -10,12 +10,24 @@ from selenium import webdriver
 
 driver = webdriver.Chrome('C:/Users/javax/Documents/Python Scripts/Basic python projects/webscraper_lucky/drivers/chromedriver.exe')
 
+
+
+
+
 def main():
+    global searchURL, searchBar
+
+    searchURL = "https://money.cnn.com/data/markets/"
+
+    # sets a 10-second timeout for the page
     driver.set_page_load_timeout(10)
     driver.get("https://google.com")
+
+    # goes to the given site (in this case i chose the stock market)
+    searchBar = driver.find_element_by_name("q")
+    searchBar.send_keys("https://money.cnn.com/data/markets/")
     
-
-
+    driver.find_element_by_name("btnK").click()
 
 main()
 
